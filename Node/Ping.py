e139c155fb955f55d331a5d32747bf51ad6e8433
Malte-Christian <scharenberg91@googlemail.com>
2015-04-21 20:24:57 +0200

    added ping

---
+++ Node/Ping.py
@@ -0,0 +1,34 @@
+import time
+from Node.NodeBehaviorBase import NodeBehaviorBase
+
+
+__author__ = 'Malte-Christian Scharenberg'
+
+
+class Ping(NodeBehaviorBase):
+    def __init__(self):
+        self.last = False
+        self.last_received_time = time.time()
+
+    def received_packet(self, src, data):
+        received_time = time.time()
+        info = Ping.decode_sender_information(data)
+
+        if info['last']:
+            self.last = True
+
+        frame_id = 1  # TODO: use frame id of incomming packet
+
+        self.node.send_packet(frame_id, data, self.src, True)
+
+    def received_status(self, frame_id, status):
+        pass
+
+    def action(self):
+        self.node.check_channel()
+
+        # Remove behavior if 'last' identifier in packet or if no packet was received for more than 10 seconds
+        return not (self.last or time.time() - self.last_received_time > 10)
+
+    def get_max_sleep_time(self):
+        return 1

fc192358db32f8a562ad96210be9b168bdb6cedd
Malte-Christian <scharenberg91@googlemail.com>
2015-04-21 20:41:24 +0200

    fix

--- Hardware/XBeeWrapper.py
+++ Hardware/XBeeWrapper.py
@@ -61,7 +61,7 @@ class XBeeWrapper(IHardware):
         if self.node is not None:
 
             data = packet['rf_data']
-            src = ord(packet['source_addr'])
+            src = ord(packet['source_addr'][1])
 
             self.node.received_packet(src, data)
 
